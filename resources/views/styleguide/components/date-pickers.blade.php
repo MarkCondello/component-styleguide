6dabb8c25111eb5e07e3817029431f9ec5e894fe
Mark C <mark@dcode.com.au>
2022-03-04 17:24:30 +1100

    -included datepicker variations

--- resources/views/styleguide/components/date-pickers.blade.php
+++ resources/views/styleguide/components/date-pickers.blade.php
@@ -6,9 +6,11 @@
         <p>We have created a wrapper for the Vue 2 Datepicker which has a hidden input to help with sending form submissions in blades. </p>
         <p>More details about the Vue2 Datepicker package can be found <a href="https://github.com/mengxiong10/vue2-datepicker" target="blank">here</a>.</p>
         <p>The wrapper package github repo which Dcode maintains can be found <a href="https://github.com/DCODE-GROUP/vue-datepicker" target="_blank">here</a>.</p>
-
         <hr>
         <h3>Demo</h3>
+        {{-- <div class="demo">
+          <datepicker-wrapper :value="{{ json_encode(Carbon\Carbon::now()->format('d/m/Y')) }}"   :is-form-input="true" name="expected_delivery"></datepicker-wrapper>
+      </div> --}}
         <div class="demo">
             <datepicker-wrapper :value="{{ json_encode([Carbon\Carbon::now()->format('d/m/Y'), Carbon\Carbon::now()->addDays(7)->format('d/m/Y')]) }}" :is-range="true" :is-form-input="true" name="expected_delivery"></datepicker-wrapper>
         </div>
@@ -24,8 +26,14 @@
 </datepicker-wrapper>'></code-block>
         <hr>
         <h3>Expected data format</h3>
-        <small>date items</small>
+        <p>The date format for the date picker us 'd/m/Y'.</p>
+        <small>single date items</small>
+        <code-block code-content='{{ json_encode(Carbon\Carbon::now()->format('d/m/Y')) }}'></code-block>
         <small>date ranges</small>
+        <code-block code-content='{{ json_encode([Carbon\Carbon::now()->format('d/m/Y'), Carbon\Carbon::now()->addDays(7)->format('d/m/Y')]) }}'></code-block>
+        <hr>
+        <h3>Returned date format</h3>
+        
         <hr>
         <h3>Configuration options</h3>
         <table>
